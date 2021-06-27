1f71a6178297c59cc2967441e59a5ad373681ae1
shashankr07 <86480671+shashankr07@users.noreply.github.com>
2021-06-27 12:40:36 +0530

    Delete label_button_size.py

--- label_button_size.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-
-root = Tk()
-root.geometry("500x500")
-frame = Frame(root, bg="blue", height=100, width=34)
-bt = Button(text="Click", height=5, width=2)
-bt.place(x=0, y=101)
-frame.place(x=0, y=0)
-root.mainloop()
